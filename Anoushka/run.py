abd1be01b098f1687fa485a9e5022b4105dc4a55
Anoushka1009 <anoushka.ruikar@gmail.com>
2024-02-01 11:54:17 +0530

    Paths added in Flask run file

--- Anoushka/run.py
+++ Anoushka/run.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, url_for
 
 app = Flask(__name__)
 
@@ -6,6 +6,18 @@ app = Flask(__name__)
 @app.route('/homepage')
 def home():
     return render_template('homepage.html')
+
+@app.route('/login')
+def login():
+    return render_template('login.html')
+
+@app.route('/register')
+def register():
+    return render_template('register.html')
+
+@app.route('/forgot_password')
+def forgot_password():
+    return render_template('forgot_password.html')
 
 if __name__ == 'main':
     app.run(debug=True)
